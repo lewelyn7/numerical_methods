e33b8e061663eb80546cc9eb2460dee31f1ca8bd
lewelyn7 <karol.hamielec@gmail.com>
2020-05-24 17:01:28 +0200

    lab7 done

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -2,6 +2,17 @@
 #include <vector>
 #include <functional>
 #include <cmath>
+#include <fstream>
+#include <string>
+#include <random>
+
+double my_abs(double x){
+    if(x > 0){
+        return x;
+    }else{
+        return -x;
+    }
+}
 
 class IIntegrationEngine
 {
@@ -120,27 +131,126 @@ class SimpsonIntegration: public IIntegrationEngine
             this->n = x;
         }
 };
+
+class MCIntegration: public IIntegrationEngine
+{
+    private:
+        int n = 1;
+    public:
+        virtual double  integrate(double a, double b, std::function<double(double)> f)
+        {
+            std::default_random_engine gen; 
+            std::uniform_real_distribution<double> distr(a,b); 
+            double dx = b-a;
+            double sum = 0;
+            for(int i = 0; i < n; i++){
+                double x = distr(gen);
+                sum += f(x);
+            }
+            return sum*dx/(double)n;
+        }
+        virtual void set_precision(int x)
+        {
+            this->n = x;
+        }
+};
+
+double pi_MC(int N){ 
+    std::default_random_engine gen; 
+    std::uniform_real_distribution<double> distr(0,1.0); 
+    int hits = 0;
+    for (int i = 0; i < N; i++) {  
+        double x = distr(gen); 
+        double y = distr(gen); 
+        double l = sqrt(x * x + y * y); 
+        if (l <= 1) hits++; 
+    } 
+    double pi = double(hits) / (double)N * 4.0;
+    return pi; 
+} 
+
 void make_tests(){
     std::function<double(double)> fs[3];
-    std::function<double(double)> fs[0] = [](double x){ return exp(x);};
+    fs[0] = [](double x){ return exp(x);};
+    fs[1] = [](double x){ return sin(x*x*x);};
+    fs[2] = [](double x){ return log(x)*x*x;};
+    double ab[3][2] = {
+        {1, 3},
+        {1, 3},
+        {1, 3}
+    };
+    double exact_values[3] = {17.367, 0.222579, 6.9986};
+
+
+
+    RectangleIntegration rect;
+    TrapeziumIntegration trap;
+    SimpsonIntegration simp;
+
 
     for(int i = 0; i < 3; i++){
-        for(int j = 5; j < 25; j+=5){
-            
+        std::ofstream frect;
+        std::ofstream ftrap;
+        std::ofstream fsimpson;
+        frect.open("rect" + std::to_string(i) + ".txt", std::ios::out);
+        ftrap.open("trap" + std::to_string(i) + ".txt", std::ios::out);
+        fsimpson.open("simpson" + std::to_string(i) + ".txt", std::ios::out);
+        for(int j = 5; j < 40; j+=5){
+            rect.set_precision(j);
+            trap.set_precision(j);
+            simp.set_precision(j);
+
+            frect << j << " " << my_abs(rect.integrate(ab[i][0], ab[i][1], fs[i]) - exact_values[i]) << std::endl;
+            ftrap << j << " " << my_abs(trap.integrate(ab[i][0], ab[i][1], fs[i]) - exact_values[i]) << std::endl;
+            fsimpson << j << " " << my_abs(simp.integrate(ab[i][0], ab[i][1], fs[i]) - exact_values[i]) << std::endl;
         }
+        frect.close();
+        ftrap.close();
+        fsimpson.close();
+    }
+
+    //Monte Carlo test
+    std::vector<double> monte_values({10, 100, 1000, 10000, 100000, 1000000, 10000000});
+    MCIntegration mcinter;
+    for(int i = 0; i < 3; i++){
+        std::ofstream fmonte;
+        fmonte.open("monte" + std::to_string(i) + ".txt", std::ios::out);
+        
+        for(int j = 0; j < monte_values.size(); j++){
+            mcinter.set_precision(monte_values[j]);
+            fmonte << j*5+5 << " " << my_abs(mcinter.integrate(ab[i][0], ab[i][1], fs[i]) - exact_values[i]) << std::endl;
+        }
+
+        fmonte.close();
     }
 
 }
-int main(){
 
-    std::function<double(double)> func1 = [](double x){ return exp(x);};
-    RectangleIntegration integration;
-    TrapeziumIntegration integration2;
-    SimpsonIntegration integration3;
-    integration.set_precision(10);
-    integration2.set_precision(10);
-    integration3.set_precision(10);
-    std::cout << func1(1) << std::endl << integration.integrate(0,3,func1) << std::endl  << integration2.integrate(0,3,func1) << std::endl  << integration3.integrate(0,3,func1);
+
+void pi_test(){
+    std::vector<double> values({10000, 100000, 1000000, 10000000, 100000000});
+    double exact_value = M_PI;
+    std::ofstream pif;
+    pif.open("pi.txt", std::ios::out);
+    for(int i = 0; i < values.size(); i++){
+        double calc_pi = pi_MC(values[i]);
+        std::cout << values[i] << "  " << calc_pi << "  " << my_abs(calc_pi - exact_value) << std::endl; 
+        pif << std::fixed << i << "  " << my_abs(calc_pi - exact_value) << std::endl; 
+    }
+    pif.close();
+}   
+int main(){
+    make_tests();
+    // pi_test();
+    
+    // std::function<double(double)> func1 = [](double x){ return exp(x);};
+    // RectangleIntegration integration;
+    // TrapeziumIntegration integration2;
+    // SimpsonIntegration integration3;
+    // integration.set_precision(10);
+    // integration2.set_precision(10);
+    // integration3.set_precision(10);
+    // std::cout << func1(1) << std::endl << integration.integrate(0,3,func1) << std::endl  << integration2.integrate(0,3,func1) << std::endl  << integration3.integrate(0,3,func1);
 
 
 }
